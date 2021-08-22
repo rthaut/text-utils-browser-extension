b285f09140b86a466e96923afb309c8ed7fab274
Ryan Thaut <rthaut@gmail.com>
2021-08-22 13:55:08 -0600

    (WIP) alternate way of managing/configuring menus

--- app/scripts/options-react-app/components/MenuAccordion.jsx
+++ app/scripts/options-react-app/components/MenuAccordion.jsx
@@ -7,75 +7,16 @@ import {
   GetDefaultMenuConfigs,
   GetDefaultMenuTitle,
   GetMenuConfigsFromStorage,
-  GetMenusAsConfigured,
+  GetMenusWithConfigs,
 } from "scripts/menus";
 
-import {
-  createTheme,
-  withStyles,
-  ThemeProvider,
-} from "@material-ui/core/styles";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import MuiAccordion from "@material-ui/core/Accordion";
-import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
-import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
-
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import SettingsBackupRestoreIcon from "@material-ui/icons/SettingsBackupRestore";
 
 import MenuList from "./MenuList";
-
-const Accordion = withStyles((theme) => ({
-  root: {
-    border: "1px solid",
-    borderColor: theme.palette.divider,
-    boxShadow: "none",
-    "&:not(:last-child)": {
-      borderBottom: 0,
-      marginBottom: -1,
-    },
-    "&:before": {
-      display: "none",
-    },
-    "&$expanded": {
-      margin: "auto",
-    },
-  },
-  expanded: {},
-}))(MuiAccordion);
-
-const AccordionSummary = withStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.default,
-    borderBottom: "1px solid",
-    borderBottomColor: theme.palette.divider,
-    marginBottom: 0,
-    minHeight: 56,
-    "&$expanded": {
-      minHeight: 56,
-    },
-  },
-  content: {
-    "&$expanded": {
-      margin: theme.spacing(1, 0),
-    },
-    display: "flex",
-    flexDirection: "column",
-    margin: theme.spacing(1, 0),
-  },
-  expanded: {},
-}))(MuiAccordionSummary);
-
-const AccordionDetails = withStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 0,
-  },
-}))(MuiAccordionDetails);
 
 const resetButtonTheme = createTheme({
   palette: {
@@ -92,84 +33,60 @@ export default function MenuAccordion() {
 
   React.useEffect(() => {
     (async () => {
-      setMenus(await GetMenusAsConfigured());
+      setMenus(await GetMenusWithConfigs());
       setConfigs(await GetMenuConfigsFromStorage());
     })();
   }, []);
 
-  const updateMenus = (group) => (newMenus) => {
+  const updateMenus = (newMenus) => {
     // update the full menu structure to keep the UI in sync
-    setMenus((prevMenus) => ({
-      ...prevMenus,
-      [group]: newMenus,
-    }));
+    setMenus({
+      ...newMenus,
+    });
 
     // update the configs structure in storage
-    setConfigs((prevConfigs) => ({
-      ...prevConfigs,
-      [group]: Object.fromEntries(
-        newMenus.map(({ id, enabled, order, title, ...menu }) => [
-          id,
-          {
-            enabled,
-            order,
-            title: title !== GetDefaultMenuTitle(id) ? title : null,
-          },
-        ])
+    setConfigs({
+      ...Object.fromEntries(
+        Object.values(newMenus).map(
+          ({ id, enabledContexts, order, title, ...menu }) => [
+            id,
+            {
+              enabledContexts,
+              order,
+              title: title !== GetDefaultMenuTitle(id) ? title : null,
+            },
+          ]
+        )
       ),
-    }));
+    });
   };
 
-  const resetMenusForGroup = async (group) => {
+  const resetMenus = async () => {
     const defaultConfigs = GetDefaultMenuConfigs();
-    setConfigs((prevConfigs) => ({
-      ...prevConfigs,
-      [group]: defaultConfigs[group],
-    }));
+    setConfigs({
+      ...defaultConfigs,
+    });
 
-    setMenus(await GetMenusAsConfigured());
+    setMenus(await GetMenusWithConfigs());
   };
 
   return (
     <>
-      {Object.keys(menus).map((group, index) => (
-        <Accordion key={index} defaultExpanded>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography color="textPrimary" variant="subtitle1">
-              {browser.i18n.getMessage(
-                `MenuGroup_${group.replace(/\-/g, "")}_Title`
-              )}
-            </Typography>
-            <Typography color="textSecondary" variant="subtitle2">
-              {browser.i18n.getMessage(
-                `MenuGroup_${group.replace(/\-/g, "")}_Description`
-              )}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <MenuList menus={menus[group]} updateMenus={updateMenus(group)} />
-            <Box display="flex" justifyContent="center" mb={2} p={1}>
-              <ThemeProvider theme={resetButtonTheme}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth={false}
-                  size="large"
-                  startIcon={<SettingsBackupRestoreIcon />}
-                  onClick={(_evt) => resetMenusForGroup(group)}
-                >
-                  {browser.i18n.getMessage(
-                    "OptionsButtonResetMenusTextWithGroupNamePlaceholder",
-                    browser.i18n.getMessage(
-                      `MenuGroup_${group.replace(/\-/g, "")}_Title`
-                    )
-                  )}
-                </Button>
-              </ThemeProvider>
-            </Box>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+      <MenuList menus={menus} updateMenus={updateMenus} />
+      <Box display="flex" justifyContent="center" mb={2} p={1}>
+        <ThemeProvider theme={resetButtonTheme}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth={false}
+            size="large"
+            startIcon={<SettingsBackupRestoreIcon />}
+            onClick={(_evt) => resetMenus()}
+          >
+            {browser.i18n.getMessage("OptionsButtonResetMenusText")}
+          </Button>
+        </ThemeProvider>
+      </Box>
     </>
   );
 }
