3d8dc95389fb4baf059940b518c38e1adec39d39
Ryan Thaut <rthaut@gmail.com>
2021-09-13 22:23:06 -0600

    ForceDarkMode setting default value; better default setting values handling

--- app/scripts/utils/settings.js
+++ app/scripts/utils/settings.js
@@ -2,12 +2,15 @@ export const SETTINGS_STORAGE_KEY = "settings";
 
 export const DEFAULT_SETTINGS = {
   EditableApplyToSelectedTextOnly: true,
+  ForceDarkMode: false,
 };
 
-export const GetSetting = async (key) => {
+export const GetSetting = async (key, defaultValue = undefined) => {
   const { [SETTINGS_STORAGE_KEY]: settings } = await browser.storage.sync.get({
-    [SETTINGS_STORAGE_KEY]: DEFAULT_SETTINGS,
+    [SETTINGS_STORAGE_KEY]: { [key]: defaultValue ?? DEFAULT_SETTINGS[key] },
   });
 
-  return settings?.[key];
+  const value = settings[key] ?? defaultValue ?? DEFAULT_SETTINGS[key];
+  // console.log(key, value);
+  return value;
 };
