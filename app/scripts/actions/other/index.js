a64972a3f2ca873db4ac3629b06c76adfa9e4087
Ryan Thaut <rthaut@gmail.com>
2021-09-15 17:24:54 -0600

    drop unicode characters from "ReverseKeepDirectionalCharacters" utility for now

--- app/scripts/actions/other/index.js
+++ app/scripts/actions/other/index.js
@@ -1,9 +1,9 @@
 export const Reverse = (text) => text.split("").reverse().join("");
 
-// TODO: there are many more unicode characters, like arrows, angled brackets, quote marks
+// TODO: there are many directional unicode characters, like arrows, angled brackets, quote marks
 // TODO: it would be nice to maintain the list just once; i.e. generate the Regex and the replacement logic from a different structure
 export const ReverseKeepDirectionalCharacters = (text) =>
-  Reverse(text).replace(/[\<\>\[\]\{\}\(\)\‘\’\“\”\‛\‚\‟\„]/g, (char) => {
+  Reverse(text).replace(/[\<\>\[\]\{\}\(\)]/g, (char) => {
     switch (char) {
       case "<": return ">"
       case ">": return "<"
@@ -13,14 +13,6 @@ export const ReverseKeepDirectionalCharacters = (text) =>
       case "}": return "{"
       case "(": return ")"
       case ")": return "("
-      case "‘": return "’"
-      case "’": return "‘"
-      case "“": return "”"
-      case "”": return "“"
-      case "‛": return "‚"
-      case "‚": return "‛"
-      case "‟": return "„"
-      case "„": return "‟"
       default: return char;
     }
   });
