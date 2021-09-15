e6e538d66e75ebde787aa408345247173608793a
Ryan Thaut <rthaut@gmail.com>
2021-09-15 17:15:19 -0600

    removed browser-specific CSS for Options page

--- app/scripts/options-page.js
+++ app/scripts/options-page.js
@@ -4,9 +4,3 @@ import ReactDOM from "react-dom";
 import OptionsReactApp from "./react-apps/OptionsApp.jsx";
 
 ReactDOM.render(<OptionsReactApp />, document.querySelector("#root"));
-
-const params = new URL(document.location).searchParams;
-const vendor = params.get("vendor");
-if (vendor) {
-  document.documentElement.classList.add(`browser-${vendor}`);
-}
