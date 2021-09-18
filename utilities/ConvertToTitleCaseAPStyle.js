85bc972880b85191785625b49e072a1b5cf90d02
Ryan Thaut <rthaut@gmail.com>
2021-09-17 19:39:28 -0600

    remove explicit lowercase conversion from AP Style TitleCase

--- utilities/ConvertToTitleCaseAPStyle.js
+++ utilities/ConvertToTitleCaseAPStyle.js
@@ -1,8 +1,6 @@
-import ConvertToLowerCase from "./ConvertToLowerCase";
 import apStyleTitleCase from "ap-style-title-case";
 
-export default (text) =>
-  apStyleTitleCase(ConvertToLowerCase(text), { keepSpaces: true });
+export default (text) => apStyleTitleCase(text, { keepSpaces: true });
 
 export const contexts = ["editable", "selection"];
 export const group = "change-case";
